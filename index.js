eec7f690806e21c78060724078f40a6056bd190b
Justin Cataldo <justin.cataldo@animoto.com>
2016-05-24 16:32:26 -0400

    allow for passing in template settings

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var _ = require('lodash');
-var loaderUtils = require("loader-utils");
+var loaderUtils = require('loader-utils');
 
-module.exports = function (source) {
+module.exports = function(source) {
   this.cacheable && this.cacheable();
   var options = loaderUtils.parseQuery(this.query);
+
+  ['escape', 'interpolate', 'evaluate'].forEach(function(templateSetting) {
+    var setting = options[templateSetting];
+    if (_.isString(setting)) {
+      options[templateSetting] = new RegExp(setting, 'g');
+    }
+  });
+
   var template = _.template(source, options);
   return 'module.exports = ' + template;
 };
