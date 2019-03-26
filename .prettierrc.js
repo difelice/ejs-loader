421b21a6ab1eaaca6782f01860ff9eab7ca6664c
Alessandro Di Felice <alessandro@uniregistry.com>
2019-03-26 10:54:05 -0500

    Fix vulnerabilities. - Change repo URL. - Add basic first unit test. - Add prettier and editor config files.

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    parser: 'babylon',
+    printWidth: 80,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'es5',
+    useTabs: false,
+    bracketSpacing: true,
+    overrides: [
+        {
+            files: '*.less',
+            options: {
+                parser: 'less',
+            },
+        },
+    ],
+};
