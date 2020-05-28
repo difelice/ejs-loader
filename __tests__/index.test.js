434a77113550a1821abeeab3c500c67cb50939d6
AtofStryker <bglesias@grove.co>
2020-05-28 16:05:58 -0400

    fix test to properly parse loader

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,12 +1,31 @@
-const requireFromString = require('require-from-string');
 const ejsLoader = require('../index.js');
 
-describe('ejsLoader', function() {
-  it('returns template with applied parameters', function() {
-    const template = '<div>Hello <%= world %>!</div>';
-    const params = { world: 'World' };
-    const compiled = requireFromString(ejsLoader(template));
+const compileTemplate = (template, mockedLoaderContext = {}) => {
+  const mergedMockedLoaderContext = {
+    cacheable: null,
+    ...mockedLoaderContext
+  };
 
+  return ejsLoader.call(mergedMockedLoaderContext, template);
+}
+
+const convertTemplateStringToFunction = (templateString) => {
+  const removeExportDefaultString = templateString.match(/export default (.*)/s)[1];
+  return new Function(`return ${removeExportDefaultString}`)();
+}
+
+describe('ejsLoader', () => {
+  it('returns template with applied parameters', () => {
+    const template = '<div>Hello <%= args.world %>!</div>';
+    const params = { 
+      world: 'World'
+    };
+    const compilerOptions = {
+      query: {
+        variable: 'args'
+      }
+    };
+    const compiled = convertTemplateStringToFunction(compileTemplate(template, compilerOptions));
     expect(compiled(params)).toBe('<div>Hello World!</div>');
   });
 });
