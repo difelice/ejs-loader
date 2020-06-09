99e11a182759519f3961be7596744957c504b0f3
Bill <bglesias@gmail.com>
2020-06-09 19:28:21 -0400

    use arrow functions within test suite to be consistent

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,8 @@
 const requireFromString = require('require-from-string');
 const ejsLoader = require('../index.js');
 
-describe('ejsLoader', function() {
-  it('returns template with applied parameters', function() {
+describe('ejsLoader', () => {
+  it('returns template with applied parameters', () => {
     const template = '<div>Hello <%= world %>!</div>';
     const params = { world: 'World' };
     const compiled = requireFromString(ejsLoader(template));
